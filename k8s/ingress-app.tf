9c1549e463b90faff839e4d72d193659159c8717
Grzegorz Banaszak <grzegorz.banaszak95@gmail.com>
2025-11-05 14:04:21 +0100

    Dodanie obsługi ingerss grpc

--- k8s/ingress-app.tf
+++ k8s/ingress-app.tf
@@ -1,33 +1,3 @@
-# # ingress-app.tf
-# resource "kubernetes_ingress_v1" "ing" {
-#   metadata {
-#     name      = "apigateway-ingress"
-#     namespace = var.namespace
-#     annotations = {
-#       "kubernetes.io/ingress.class" = "nginx"
-#     }
-#   }
-#   spec {
-#     rule {
-#       host = var.host
-#       http {
-#         path {
-#           path      = "/"
-#           path_type = "Prefix"
-#           backend {
-#             service {
-#               name = kubernetes_service_v1.apigateway.metadata[0].name
-#               port { number = 80 }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-#   depends_on = [kubernetes_namespace_v1.ns]
-# }
-
-
 # ingress-gateway.tf
 resource "kubernetes_ingress_v1" "ing" {
   metadata {
@@ -37,6 +7,7 @@ resource "kubernetes_ingress_v1" "ing" {
       # Jeśli używasz starszych manifestów/classic:
       "kubernetes.io/ingress.class" = "nginx"
       # (opcjonalnie) przykładowe tuningi NGINX:
+      "nginx.ingress.kubernetes.io/backend-protocol"   = "GRPC"
       "nginx.ingress.kubernetes.io/proxy-read-timeout" = "60"
       "nginx.ingress.kubernetes.io/proxy-send-timeout" = "60"
     }
